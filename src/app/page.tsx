0b3465f813be76c3e84a4d39f763fdc617e1e3c5
후디 이야기Hoodie’s Story <shinm93@gmail.com>
2024-01-10 00:02:21 +0900

    서버 컴포넌트와 클라이언트 컴포넌트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Counter from "@/components/Counter";
 import { Inter } from "@next/font/google";
+import os from "os";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>홈페이지!!!</h1>;
+  console.log("hi");
+  console.log(os.hostname());
+  return (
+    <>
+      <h1>홈페이지다!!</h1>
+      <Counter />
+    </>
+  );
 }
